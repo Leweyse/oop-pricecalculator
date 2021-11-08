7d525d4968d963ce5d53918ce05ec61ba1a6a9b0
Leweyse <darylcastro67@gmail.com>
2021-11-08 14:59:38 +0100

    added new files to index.php

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
-require "Helper/Connection.php";
-require "Model/Product.php";
-include 'Helper/DotEnv.php';
 
-$post = new DotEnv(__DIR__ . '/.env');
-$post -> load();
+require "Model/Discount.php";
+require "Model/Customer.php";
+require "Model/CustomerGroup.php";
+require "Model/Product.php";
+
+require "Helper/Connection.php";
+require 'Helper/DotEnv.php';
+
+$env = new DotEnv(__DIR__ . '/.env');
+$env -> load();
+
 $password = getenv('PASSWORD');
 $username = getenv('USERNAME');
 $database = getenv('DATABASE');
 $hostname = getenv('HOSTNAME');
 
 $conn = new Connection($hostname, $username, $password, $database);
+
 $colLength = (int)$conn->getColLength("product");
 
 for ($i = 1; $i <= $colLength; $i++) {
-    var_dump($conn->displayProduct($i));
+    var_dump($conn -> displayProduct($i));
 }
 
