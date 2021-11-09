5d7308fc85d907325daac0dcb2b5bbf4a3513e9e
jvondzerza <jvondzerza@googlemail.com>
2021-11-09 16:29:51 +0100

    updated to do for tomorrow, mostly functional stuff to calculate optimal discount

--- Controller/POST.php
+++ Controller/POST.php
@@ -1,4 +1,13 @@
 <?php
+//TODO
+//with the value of $POST, we check the id of the customer
+//use id of the customer to check fixed and variable discounts and group id
+//use group id to check fixed and variable discounts and parent id
+//while parent id!=null, use parent id to check fixed and variable discounts of parent group
+//add sum of fixed discounts to highest value of variable discount
+//deduct discount from item price
+//if item price < 0, item price = 0
+
 $customer = new Customer();
 $product = new Product();
 $customerGroup = new CustomerGroup();
