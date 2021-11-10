caa793ad44bd6493c2b14e4ebee289c29e840f84
Leweyse <darylcastro67@gmail.com>
2021-11-10 11:29:36 +0100

    implemented discount-class

--- Controller/POST.php
+++ Controller/POST.php
@@ -8,8 +8,26 @@
 //deduct discount from item price
 //if item price < 0, item price = 0
 
-$customer = new Customer();
-$product = new Product();
+$discount = new Discount();
 
-$data->setAllProducts($product);
-$data->setAllCustomers($customer);
+$data->setAllProducts();
+$data->setAllCustomers();
+
+$productId = $customerId = null;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST['idProduct']) && isset($_POST['idCustomer'])) {
+        $productId = $_POST['idProduct'];
+        $customerId = $_POST['idCustomer'];
+
+        var_dump($data -> setProduct($productId));
+        var_dump($data -> setCustomer($customerId));
+        var_dump($data -> setCustomerGroup($customerId));
+
+        var_dump($data -> getProduct());
+        var_dump($data -> getCustomer());
+        var_dump($data -> getCustomerGroup());
+    }
+}
+
+var_dump($productId, $customerId);
--- Model/Data.php
+++ Model/Data.php
@@ -7,34 +7,76 @@ class Data
 
     private array $allCustomers;
 
+    private array $arrProduct;
+
+    private array $arrCustomer;
+
+    private array $arrCustomerGroup;
+
+    private Customer $customer;
+    
+    private Product $product;
+
+    private Discount $discount;
+
     private Connection $conn;
 
     public function __construct($conn)
     {
         $this->conn = $conn;
+
+        $this->product = new Product();
+
+        $this->customer = new Customer();
+
+        $this->discount = new Discount();
     }
 
-    public function setAllProducts($products): void
+    public function setAllProducts(): void
     {
         $colLength = (int)$this->conn->getColLength("product");
 
         for ($i = 1; $i <= $colLength; $i++) {
-          $this->conn->setData($i, "product", ["id", "name", "price"], $products);
+          $this->conn->setData($i, "product", ["id", "name", "price"], $this->product);
         }
 
         $this->allProducts = $this->conn->getData();
         $this->conn->clearData();
     }
 
-    public function setAllCustomers($customers): void
+    public function setProduct($id): void
+    {
+        $this->conn->setData($id, "product", ["id", "name", "price"], $this->product);
+
+        $this->arrProduct = $this->conn->getData();
+        $this->conn->clearData();
+    }
+
+    public function setAllCustomers(): void
     {
         $colLength = (int)$this->conn->getColLength("customer");
 
         for ($i = 1; $i <= $colLength; $i++) {
-            $this->conn->setData($i, "customer", ["id", "firstname", "lastname"], $customers);
+            $this->conn->setData($i, "customer", ["id", "firstname", "lastname"], $this->customer);
         }
 
         $this->allCustomers = $this->conn->getData();
+        $this->conn->clearData();
+    }
+
+    public function setCustomer($id): void
+    {
+        $this->conn->setData($id, 'customer', ["group_id", "fixed_discount", "variable_discount"], $this->discount);
+
+        $this->arrCustomer = $this->conn->getData();
+        $this->conn->clearData();
+    }
+
+    public function setCustomerGroup($id): void
+    {
+        $this->conn->setData($id, 'customer_group', ["parent_id", "fixed_discount", "variable_discount"], $this->discount);
+
+        $this->arrCustomerGroup = $this->conn->getData();
         $this->conn->clearData();
     }
 
@@ -43,8 +85,23 @@ class Data
         return $this->allProducts;
     }
 
+    public function getProduct(): array
+    {
+        return $this->arrProduct;
+    }
+
     public function getAllCustomers(): array
     {
         return $this->allCustomers;
+    }
+
+    public function getCustomer(): array
+    {
+        return $this->arrCustomer;
+    }
+
+    public function getCustomerGroup(): array
+    {
+        return $this->arrCustomerGroup;
     }
 }
